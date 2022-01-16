d905e994e1cc5120caa97c4ee9b78e90b74f5eff
Abdullah M. Al khareji <akhareji@cc.mof.gov.kw>
2022-01-16 10:41:45 +0300

    did something

--- bareed.js
+++ bareed.js
@@ -10,24 +10,24 @@
  * let point = new Point(x, y);
  ****************************************************************/
 class Point {
-	constructor(x, y) {
-		this.x = x;
-		this.y = y;
-	}
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
 
-	distanceTo = point => {
-		let xDelta = this.x - point.x;
-		let yDelta = this.y - point.y;
-		return Math.sqrt(xDelta * xDelta + yDelta * yDelta); // PYTHAGORAS!
-	};
+  distanceTo = (point) => {
+    let xDelta = this.x - point.x;
+    let yDelta = this.y - point.y;
+    return Math.sqrt(xDelta * xDelta + yDelta * yDelta); // PYTHAGORAS!
+  };
 
-	equals = point => point.x === this.x && point.y === this.y;
+  equals = (point) => point.x === this.x && point.y === this.y;
 
-	static randomPoint = (maxX, maxY) => {
-		let x = Math.random() * (maxX || 100);
-		let y = Math.random() * (maxY || 100);
-		return new Point(x, y);
-	};
+  static randomPoint = (maxX, maxY) => {
+    let x = Math.random() * (maxX || 100);
+    let y = Math.random() * (maxY || 100);
+    return new Point(x, y);
+  };
 }
 
 /**********************************************************
@@ -42,18 +42,18 @@ class Point {
  * let wallet = new Wallet(money);
  **********************************************************/
 class Wallet {
-	// implement Wallet!
-	constructor(money = 0) {
-		this.money = money;
-	}
+  // implement Wallet!
+  constructor(money = 0) {
+    this.money = money;
+  }
 
-	credit = amount => {
-		this.money = this.money + amount;
-	};
+  credit = (amount) => {
+    this.money = this.money + amount;
+  };
 
-	debit = amount => {
-		this.money = this.money - amount;
-	};
+  debit = (amount) => {
+    this.money = this.money - amount;
+  };
 }
 
 /**********************************************************
@@ -68,16 +68,16 @@ class Wallet {
  * let person = new Person(name, x, y);
  **********************************************************/
 class Person {
-	// implement Person!
-	constructor(name, x, y) {
-		this.name = name;
-		this.location = new Point(x, y);
-	}
-	wallet = new Wallet();
+  // implement Person!
+  constructor(name, x, y) {
+    this.name = name;
+    this.location = new Point(x, y);
+  }
+  wallet = new Wallet();
 
-	moveTo = point => {
-		this.location = point;
-	};
+  moveTo = (point) => {
+    this.location = point;
+  };
 }
 
 /**********************************************************
@@ -96,17 +96,15 @@ class Person {
  * new vendor = new Vendor(name, x, y);
  **********************************************************/
 class Vendor extends Person {
-	// implement Vendor!
-	constructor(name, x, y, range = 5, price = 1) {
-		super(name);
-		super(x);
-		super(y);
-		this.range = range;
-		this.price = price;
-	}
-	// sellTo = (customer, numberOfIceCreams) => {
-	// 	customer.moveTo(this.location);
-	// };
+  // implement Vendor!
+  constructor(name, x, y, range = 5, price = 1) {
+    super(name, x, y);
+    this.range = range;
+    this.price = price;
+  }
+  // sellTo = (customer, numberOfIceCreams) => {
+  // 	customer.moveTo(this.location);
+  // };
 }
 
 /**********************************************************
@@ -126,13 +124,14 @@ class Vendor extends Person {
  * new customer = new Customer(name, x, y);
  **********************************************************/
 class Customer extends Person {
-	// implement Customer!
-	constructor(name, x, y) {
-		super(name);
-		super(x);
-		super(y);
-	}
-	wallet = new Wallet((money = 10));
+  // implement Customer!
+  constructor(name, x, y) {
+    super(name, x, y);
+  }
+  wallet = new Wallet((money = 10));
+  _isInRange = (vendor) => {
+	
+  }
 }
 export { Point, Wallet, Person, Customer, Vendor };
 
